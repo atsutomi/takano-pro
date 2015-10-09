ac231b4739600d5e74d4d2f477442f68e8211e59
ne250250 <ne250250@senshu-u.jp>
2015-10-09 19:57:28 +0900

    index

--- hisatomi/anatock/app/controllers/stocks_controller.rb
+++ hisatomi/anatock/app/controllers/stocks_controller.rb
@@ -2,7 +2,19 @@
 
 class StocksController < ApplicationController
   def index
-    @stock = Stock.find(1)
+    @page = 1
+    @pages = [1,2,3,4,5]
+    @stocks = Stock.order("num")
+    @Pstocks = @stocks.limit(20)
+    @market = Stock.select("market").uniq
+  end
+  
+  
+  def search
+    @Pstocks = Stock.search(params[:q])
+    @market = Stock.select("market").uniq
+    @pages = [1,2,3,4,5]
+    render "index"
   end
   
   def show
@@ -23,4 +35,40 @@ class StocksController < ApplicationController
       
     end
   end
+  
+  def page
+    @pages = [1,2,3,4,5]
+    @page = params[:id]
+    @pagei = @page.to_i
+    if 0 < @pagei
+    @count = 20 * (@page.to_i - 1)
+    @stocks = Stock.order("num")
+    @Pstocks2 = @stocks.offset(@count)
+    @Pstocks = @Pstocks2.limit(20)
+    @market = Stock.select("market").uniq
+    else
+    @pagei = 1
+    @count = 20 * (@page.to_i - 1) 
+    @stocks = Stock.order("num")
+    @Pstocks = @stocks.offset(@count).limit(20)
+    #@Pstocks = @Pstocks2.limit(20)
+    @market = Stock.select("market").uniq
+    end
+    
+    if 3 < @pagei
+      @n = @pagei - 2
+      for @i in 1..5 do
+        @pages[@i-1] = @n
+        @n += 1
+      end
+    else
+      @n = 1
+      for @i in 1..5 do
+        @pages[@i-1] = @n
+        @n += 1
+      end
+    end
+    render "index"
+  end
+  
 end
--- hisatomi/anatock/config/routes.rb
+++ hisatomi/anatock/config/routes.rb
@@ -2,6 +2,7 @@ Anatock::Application.routes.draw do
   root to: "ranking#index"
   resources :stocks, only: [:index, :show] do
     collection { get "search" }
+    collection { get "page" }
   end
   resources :ranking, only: [:index]
 end
